179082d5f6f610f3c032bba695b5cb64dd79e630
arturobl00 <arturobl00@msn.com>
2021-03-09 11:42:41 -0600

    Agregar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, {useState} from 'react';
 import UserTable from './componentes/UserTable';
-
+import { v4 as uuidv4 } from 'uuid';
+import AddUserForm from './componentes/AddUserForm';
 
 function App() {
 
   const usersData = [
-    { id: 1, name: 'Tania', username: 'floppydiskette' },
-    { id: 2, name: 'Craig', username: 'siliconeidolon' },
-    { id: 3, name: 'Ben', username: 'benisphere' },
-    { id: 4, name: 'Lupita', username: 'lupita_00'}
+    { id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+    { id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+    { id: uuidv4(), name: 'Ben', username: 'benisphere' },
+    { id: uuidv4(), name: 'Lupita', username: 'lupita_00'}
   ]
 
   //Nota simpre que usemos useState hay que ver que se este agregando en la lib de react
   const [users, setUsers] = useState(usersData);
   
+  //Agregar usuarios
+  const addUser = (user) => {
+    user.id = uuidv4()
+    setUsers([
+      ...users,
+      user
+    ])
+  }
 
   return (
     <div className="container">
@@ -21,6 +30,7 @@ function App() {
       <div className="flex-row">
       <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser}/>
         </div>
         <div className="flex-large">
           <h2>View users</h2>
--- src/componentes/UserTable.jsx
+++ src/componentes/UserTable.jsx
@@ -14,6 +14,7 @@ const UserTable = (props) => {
             </thead>
             <tbody>
             {
+                    props.users.length > 0 ?
                     props.users.map(user => (
                         <tr key={user.id}>
                             <td>{user.name}</td>
@@ -23,7 +24,12 @@ const UserTable = (props) => {
                             <button className="button muted-button">Delete</button>
                             </td>
                         </tr>
-                    ))
+                    )) : (
+                        <tr>
+                            <td colSpan={3}>No users</td>
+                        </tr>
+                    )
+
             }
             </tbody>
         </table>
