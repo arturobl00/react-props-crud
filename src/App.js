bbf3a1010ce2a3af6b95d631c1f9b9617f90f830
arturobl00 <arturobl00@msn.com>
2021-03-23 10:30:51 -0600

    Terminado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import UserTable from './componentes/UserTable';
 import { v4 as uuidv4 } from 'uuid';
 import AddUserForm from './componentes/AddUserForm';
+import EditUserForm from './componentes/EditUserForm';
 
 function App() {
 
@@ -31,17 +32,51 @@ function App() {
     setUsers(users.filter(user => user.id != id))
   }
 
+  //Editar usuario
+  const [bandera, setbandera] = useState(false);
+  const [currentUser, setcurrentUser] = useState({
+    id: null,
+    name: '',
+    username: ''
+  });
+
+  const editRow = (user) =>{
+    setbandera(true);
+    setcurrentUser({
+      id: user.id, name: user.name, username: user.username
+    })
+  }
+
+  const updateUser = (id, updateUser) => {
+    setbandera(false);
+    setUsers(users.map(user => (user.id === id ? updateUser : user)))
+  }
+
   return (
     <div className="container">
       <h1>Proyecto CRUD</h1>
       <div className="flex-row">
       <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser}/>
+          {
+            bandera ? (
+            <div>
+              <h2>Edit user</h2>
+              <EditUserForm currentUser={currentUser} updateUser={updateUser}/>
+            </div>
+            ) : (
+              <div>
+                <h2>Add user</h2>
+                <AddUserForm addUser={addUser}/>
+              </div>
+            )
+          }
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable users={users} deleteUser={deleteUser}/>
+          <UserTable 
+          users={users} 
+          deleteUser={deleteUser} 
+          editRow={editRow}/>
         </div>
       </div>
     </div>
